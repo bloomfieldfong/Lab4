188f0f971bbcfa4c26dddb16e5a9ba30867455f5
toshibas <toshibas@Bloomfield>
2016-10-14 09:18:43 -0600

    hola

--- AlarmaMain.java
+++ AlarmaMain.java
@@ -1,6 +1,6 @@
 // CC2006 Lab
 // Este programa cliente prueba el comportamiento de su clase AlarmaEmergencia.
-
+//
 public class AlarmaMain {
 	public static void main(String[] args) {
 /*Alarma miReloj = new Alarma();
