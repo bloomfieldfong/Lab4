56a54d6b18b7c7104d4ca78e25a06261505112ae
Michelle Bloomfield <Michelle Bloomfield>
2016-10-14 09:02:56 -0600

    Inicio

---
+++ AlarmaMain.java
@@ -0,0 +1,14 @@
+// CC2006 Lab
+// Este programa cliente prueba el comportamiento de su clase AlarmaEmergencia.
+
+public class AlarmaMain {
+	public static void main(String[] args) {
+Alarma miReloj = new Alarma();
+System.out.println(miReloj);
+miReloj.m1();
+miRelojm2();
+
+	}
+
+}
+	
---
+++ Timbre.java
@@ -0,0 +1,12 @@
+public class Timbre {
+	public void m1() {
+		System.out.println("timbre 1");
+	}
+
+	public void m2() {
+	}
+
+	public String to String() {
+		return "Ring…Ring";
+	}
+}
